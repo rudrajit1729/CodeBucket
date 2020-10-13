1af638969166968f98029c847a12d99f4abe0050
abhijit <abhidutta338@gmail.com>
2020-10-13 23:38:21 +0530

    added hackerrank java oops

---
+++ CP_Bucket/HackerRank/Practice/Java/OOPs/java_interface.java
@@ -0,0 +1,39 @@
+import java.util.*;
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+
+//Write your code here
+class MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n){
+        int sum=1;
+        for(int i=2;i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
