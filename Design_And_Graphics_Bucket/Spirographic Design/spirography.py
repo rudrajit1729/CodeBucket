fa094ae36e3d252409e08c6ac3be303cf956bd7d
Souvik Dutta <55849163+souvik199813@users.noreply.github.com>
2020-10-14 15:11:01 +0530

    Update spirography.py

--- Design_And_Graphics_Bucket/Spirographic Design/spirography.py
+++ Design_And_Graphics_Bucket/Spirographic Design/spirography.py
@@ -1,77 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 14 10:11:01 2020
-
-@author: SOUVIK
-"""
+# author - Souvik Dutta
 
 import turtle
 import math
 import random
-wn = turtle.Screen()
-wn.bgcolor('black')
-Albert = turtle.Turtle()
-Albert.speed(0)
-Albert.color('white')
-rotate=int(360)
-def drawCircles(t,size):
-    for i in range(10):
-        t.circle(size)
-        size=size-4
-def drawSpecial(t,size,repeat):
-  for i in range (repeat):
-    drawCircles(t,size)
-    t.right(360/repeat)
-drawSpecial(Albert,100,10)
-Steve = turtle.Turtle()
-Steve.speed(0)
-Steve.color('yellow')
-rotate=int(90)
-def drawCircles(t,size):
-    for i in range(4):
-        t.circle(size)
-        size=size-10
-def drawSpecial(t,size,repeat):
-    for i in range (repeat):
-        drawCircles(t,size)
-        t.right(360/repeat)
-drawSpecial(Steve,100,10)
-Barry = turtle.Turtle()
-Barry.speed(0)
-Barry.color('blue')
-rotate=int(80)
-def drawCircles(t,size):
-    for i in range(4):
-        t.circle(size)
-        size=size-5
-def drawSpecial(t,size,repeat):
-    for i in range (repeat):
-        drawCircles(t,size)
-        t.right(360/repeat)
-drawSpecial(Barry,100,10)
-Terry = turtle.Turtle()
-Terry.speed(0)
-Terry.color('orange')
-rotate=int(90)
-def drawCircles(t,size):
-    for i in range(4):
-        t.circle(size)
-        size=size-19
-def drawSpecial(t,size,repeat):
-    for i in range (repeat):
-        drawCircles(t,size)
-        t.right(360/repeat)
-drawSpecial(Terry,100,10)
-Will = turtle.Turtle()
-Will.speed(0)
-Will.color('pink')
-rotate=int(90)
-def drawCircles(t,size):
-    for i in range(4):
-        t.circle(size)
-        size=size-20
-def drawSpecial(t,size,repeat):
-    for i in range (repeat):
-        drawCircles(t,size)
-        t.right(360/repeat)
-drawSpecial(Will,100,10)
+
+win = turtle.Screen()
+win.bgcolor("black")
+
+colors = ["white", "yellow", "blue", "orange", "pink"]
+decrement = [4, 10, 5, 19, 20]
+
+def drawCircles(t, size, ind):
+	dec = decrement[ind]
+	L = 4
+	if ind == 0:
+		L = 10
+	for i in range(L):
+		t.circle(size)
+		size -= dec
+
+def drawSpecial(t, size, repeat, ind):
+	for i in range(repeat):
+		drawCircles(t, size, ind)
+		t.right(360/repeat)
+
+for i in range(len(colors)):
+	col = colors[i]
+	obj = turtle.Turtle()
+	obj.speed(0)
+	obj.color(col)
+	drawSpecial(obj, 100, 10, i)
+
+
+
