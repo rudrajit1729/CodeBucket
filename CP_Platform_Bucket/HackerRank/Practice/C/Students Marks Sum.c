2325d59fc1f71673b759dc26853d621788950fca
nilanjana2008 <47620612+nilanjana2008@users.noreply.github.com>
2020-10-16 20:14:49 +0530

    Added Python and C  Hackerrank Pratice Files

---
+++ CP_Platform_Bucket/HackerRank/Practice/C/Students Marks Sum.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+       int s = 0, i = 0;
+    if (gender == 'g') {
+        i++;
+    }
+    for (; i < number_of_students; i = i+2) {
+        s += marks[i];       
+    }
+    return s;
+}
+  
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
---
+++ CP_Platform_Bucket/HackerRank/Practice/Python/Triangle Quest 2.py
@@ -0,0 +1,6 @@
+"""
+Submission link:
+https://www.hackerrank.com/challenges/triangle-quest-2/submissions/code/179566736
+"""
+for i in range(1,int(input())+1): 
+    print ((pow(10,i)//9)**2)
