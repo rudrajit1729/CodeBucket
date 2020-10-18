94a9a76a972c87823c1a640e6a319dbc8917c2f0
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-10-18 13:00:38 +0900

    added LIS_anti_pattern algo to Dynamic Programming in algo bucket

--- Algo_Bucket/Dynamic Programming/longest_increasing_subsequence_anti_pattern.py
+++ Algo_Bucket/Dynamic Programming/longest_increasing_subsequence_anti_pattern.py
@@ -8,7 +8,7 @@ class Dp:
     def __init__(self,seq:list):
         self._seq = seq
 
-    # Get the longest subsequence element
+    # Get the longest subsequence list
     def lis(self) -> list:
         n = len(self._seq)
         LIS = [1]*n
@@ -16,22 +16,40 @@ class Dp:
             for j in range(i):
                 if self._seq[j] < self._seq[i]:
                     LIS[i] = max(LIS[i], LIS[j]+1)
-        return self.restore(LIS)
+        return self.__restore(LIS)
             
     
     # Restoration of longest subsequence
-    def restore(self, LIS:list) -> list:
-        RESTORE = deque()
-        m = len(LIS)-2
-        for i in range(len(self._seq)-1, -1, -1):
-            if LIS[m] <= self._seq[i] < LIS[m+1]:
-                RESTORE.appendleft(self._seq[i])
-                # Update longest common subsequence element
-                LIS[m] = self._seq[i]
-                m -= 1
-        return list(RESTORE)
+    def __restore(self, LIS:list) -> list:
+        m = max(LIS)
+        index = LIS.index(m)
+        RESTORE = [0]*m
+        while 0 < m:
+            for i in range(index, -1, -1):
+                if LIS[i] == m:
+                    RESTORE[m-1] = self._seq[i]
+                    index = i
+                    m -= 1
+                    break
+        return RESTORE
+            
 
 data = [19, 11, 10, 7, 8, 9, 17, 1, 20, 4, 3, 15, 16, 1, 5, 14, 6, 2, 13, 12]
 l = Dp(data)
 result = l.lis()
-print(result)
+print(result) #[7, 8, 9, 17, 20]
+
+
+# lis()
+# data 
+# [19, 11, 10, 7, 8, 9, 17, 1, 20, 4, 3, 15, 16, 1, 5, 14, 6, 2, 13, 12]
+# Subsequence length
+# [1, 1, 1, 1, 2, 3, 4, 1, 5, 2, 2, 4, 5, 1, 3, 4, 4, 2, 5, 5]
+
+# restore()
+# [0, 0, 0, 0, 20]
+# [0, 0, 0, 17, 20]
+# [0, 0, 9, 17, 20]
+# [0, 8, 9, 17, 20]
+# [7, 8, 9, 17, 20]
+# [7, 8, 9, 17, 20]
